1336dfeab0b37e7819172f9bb645f0f12a8865e7
Hryhoriy Starykov <starykovhryhoriy@gmail.com>
2018-08-12 22:03:57 +0000

    fixed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 document.getElementById('myBtnH')
     .addEventListener('click', function () {
-    fetch('data.html?r='+Math.random())
+    fetch('history.html?r='+Math.random())
         .then( response => response.text() )
         .then( modal => document.getElementById('placeholderH').innerHTML = modal );
 });
 
 document.getElementById('myBtnC')
     .addEventListener('click', function() {
-    fetch('data.html?r='+Math.random())
+    fetch('cellar.html?r='+Math.random())
         .then( response => response.text() )
         .then( modal => document.getElementById('placeholderC').innerHTML = modal );
 });
 
 document.getElementById('myBtnW')
     .addEventListener('click', function() {
-    fetch('data.html?r='+Math.random())
+    fetch('wines.html?r='+Math.random())
         .then( response => response.text() )
         .then( modal => document.getElementById('placeholderW').innerHTML = modal );
 });
